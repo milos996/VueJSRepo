81f9f919ecbb6f8c4e51271d956615a6fc9d95ec
milos996 <ivankovicmilos5@gmail.com>
2018-07-27 11:15:22 +0200

    #MIAE-22 ubacen laravel CORS

---
+++ VueJSApp/app/Http/Controllers/MyRegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class MyRegisterController extends Controller
+{
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+              'name' => 'bail|required|string|max:255',
+              'email' => 'required|email|max:255|unique:users',
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        $token = auth()->login($user);
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+
+}
