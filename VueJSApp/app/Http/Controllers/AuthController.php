995f06e637711d7f90e694c5686bc45a5a8e538b
milos996 <ivankovicmilos5@gmail.com>
2018-07-30 14:59:00 +0200

    Nista

--- VueJSApp/app/Http/Controllers/AuthController.php
+++ VueJSApp/app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
   public function login()
   {
       $credentials = request(['email', 'password']);
-
+      \Log::info('ahah');
       if (! $token = auth()->attempt($credentials)) {
           return response()->json(['error' => 'Unauthorized'], 401);
       }
